f3c42a7eb6153609c0ebac02cf0b272e8b815df0
Nate <nate@nate-web3.com>
2026-01-03 22:00:39 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const Footer = () => (
   <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
-    © 2022 Searchify, Inc.
+    © since 2022 Searchify, Inc. by Nate
   </div>
 );
